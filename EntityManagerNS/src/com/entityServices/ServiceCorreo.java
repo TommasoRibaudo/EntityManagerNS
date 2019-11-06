6a9abcebcf450dea4ea66d29a53e00a5bb40832c
Asus <Asus@DESKTOP-I39974Q>
2019-11-06 12:48:10 -0600

    Little Update
    completed missing service methods, now ready to deploy as .JAR

--- EntityManagerNS/src/com/entityServices/ServiceCorreo.java
+++ EntityManagerNS/src/com/entityServices/ServiceCorreo.java
@@ -1,39 +1,119 @@
 package com.entityServices;
 
 import java.util.List;
-import java.util.Set;
+import javax.persistence.TypedQuery;
+import com.entities.Correo;
 
+@SuppressWarnings("finally")
 public class ServiceCorreo extends Service {
+	private TypedQuery<Correo> query;
+	private Correo correo;
 
 	@Override
-	public List<?> selectAll() {
-		// TODO Auto-generated method stub
+	public List<Correo> selectAll() 
+	{
+		try {
+			this.startEntityManagerFactory();
+			this.query = this.getEm().createNamedQuery("Correo.findAll", Correo.class);
+			return query.getResultList();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		} finally {
+			this.stopEntityManagerFactory();
+			
+		}
 		return null;
 	}
 
+	
 	@Override
-	public Object selectRegister(int id) {
-		// TODO Auto-generated method stub
-		return null;
+	public Object selectRegister(int id) 
+	{
+		try {
+			this.startEntityManagerFactory();
+			this.correo = this.getEm().createNamedQuery("Correo.findOne", Correo.class).
+					setParameter("param", id).getSingleResult();
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			this.stopEntityManagerFactory();
+			return this.correo;
+		}
+
 	}
 
 	@Override
-	public void update(Object o) {
-		// TODO Auto-generated method stub
+	public void update(Object o) 
+	{
+		if (o instanceof Correo)
+		{
+			try 
+			{
+				this.startEntityManagerFactory();
+
+				this.getEm().getTransaction().begin();
+				this.getEm().merge(o);
+				this.getEm().getTransaction().commit();
+
+				this.stopEntityManagerFactory();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else 
+			System.out.print("Trying to use an invalid object, error.");
 		
 	}
 
 	@Override
-	public void delete(Object o) {
-		// TODO Auto-generated method stub
-		
+	public void delete(Object o) 
+	{
+		if (o instanceof Correo)
+		{
+			try {
+				this.startEntityManagerFactory();
+
+				this.getEm().getTransaction().begin();
+				this.getEm().remove(this.getEm().contains(o) ? o : this.getEm().merge(o));
+				this.getEm().getTransaction().commit();
+
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				this.stopEntityManagerFactory();
+			}
+		} else 
+			System.out.print("Trying to use an invalid object, error.");
 	}
 
 	@Override
-	public void insert(Object o) {
-		// TODO Auto-generated method stub
+	public void insert(Object o) 
+	{
+		if (o instanceof Correo)
+		{
+			try 
+			{
+				this.startEntityManagerFactory();
+				
+				this.getEm().getTransaction().begin();
+				this.getEm().persist(o);
+				this.getEm().getTransaction().commit();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				this.stopEntityManagerFactory();
+			}
+		} else 
+			System.out.print("Trying to use an invalid object, error.");
 		
 	}
+
 
 
 }
--- EntityManagerNS/src/com/entityServices/ServiceUsuario.java
+++ EntityManagerNS/src/com/entityServices/ServiceUsuario.java
@@ -2,38 +2,116 @@ package com.entityServices;
 
 import java.util.List;
 
+import javax.persistence.TypedQuery;
+
+import com.entities.Destinatario;
 import com.entities.Usuario;
 
+@SuppressWarnings("finally")
 public class ServiceUsuario extends Service{
+	private TypedQuery<Usuario> query;
+	private Usuario usuario;
 
 	@Override
 	public List<Usuario> selectAll() {
-		// TODO Auto-generated method stub
+		try {
+			this.startEntityManagerFactory();
+			this.query = this.getEm().createNamedQuery("Usuario.findAll", Usuario.class);
+			return query.getResultList();
+		} catch (Exception e) {
+			
+			e.printStackTrace();
+		} finally {
+			this.stopEntityManagerFactory();
+			
+		}
 		return null;
 	}
 
 	@Override
 	public Object selectRegister(int id) {
-		// TODO Auto-generated method stub
-		return null;
+		try {
+			this.startEntityManagerFactory();
+			this.usuario = this.getEm().createNamedQuery("Usuario.findOne", Usuario.class).
+					setParameter("param", id).getSingleResult();
+			
+			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			this.stopEntityManagerFactory();
+			return this.usuario;
+		}
+
 	}
 
 	@Override
 	public void update(Object o) {
-		// TODO Auto-generated method stub
-		
+		if (o instanceof Usuario)
+		{
+			try 
+			{
+				this.startEntityManagerFactory();
+
+				this.getEm().getTransaction().begin();
+				this.getEm().merge(o);
+				this.getEm().getTransaction().commit();
+
+				this.stopEntityManagerFactory();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else 
+			System.out.print("Trying to use an invalid object, error.");
+				
 	}
 
 	@Override
 	public void delete(Object o) {
-		// TODO Auto-generated method stub
-		
+		if (o instanceof Usuario)
+		{
+			try {
+				this.startEntityManagerFactory();
+
+				this.getEm().getTransaction().begin();
+				this.getEm().remove(this.getEm().contains(o) ? o : this.getEm().merge(o));
+				this.getEm().getTransaction().commit();
+
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				this.stopEntityManagerFactory();
+			}
+		} else 
+			System.out.print("Trying to use an invalid object, error.");
+			
 	}
 
 	@Override
 	public void insert(Object o) {
-		// TODO Auto-generated method stub
-		
+		if (o instanceof Usuario)
+		{
+			try 
+			{
+				this.startEntityManagerFactory();
+				
+				this.getEm().getTransaction().begin();
+				this.getEm().persist(o);
+				this.getEm().getTransaction().commit();
+				
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} finally {
+				this.stopEntityManagerFactory();
+			}
+		} else 
+			System.out.print("Trying to use an invalid object, error.");
+				
 	}
+
+	
 
 }
